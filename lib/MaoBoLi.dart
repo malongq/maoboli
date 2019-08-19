4c7fd86510d95fdc243edb334fca8976e6418952
马龙 <malong@100tal.com>
2019-08-19 14:11:59 +0800

    再次练习高斯模糊效果

--- lib/MaoBoLi.dart
+++ lib/MaoBoLi.dart
@@ -1,66 +1,96 @@
-import 'package:flutter/material.dart';
-import 'dart:ui';//引入ui库，因为 ImageFilter Wiget 需要
-
-class MaoBoLi extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('毛玻璃效果'),),
-//      body: Stack(//重叠的Stack组件，可以实现上面盖着好几层
+//import 'package:flutter/material.dart';
+//import 'dart:ui';//引入ui库，因为 ImageFilter Wiget 需要
+//
+//class MaoBoLi extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('毛玻璃效果'),),
+////      body: Stack(//重叠的Stack组件，可以实现上面盖着好几层
+////        children: <Widget>[
+////          ConstrainedBox(//约束盒子组件，可以添加额外的限制条件到child上
+////            constraints: const BoxConstraints.expand(),//限制条件，可限制child的宽和高等
+////            //child: Image.network('https://img.lovebuy99.com/uploads/allimg/190807/15-1ZPGF354-50.jpg'),
+////            child: Image.network('http://qiniu.nightfarmer.top/恶龙咆哮.gif'),
+////          ),
+////          Center(
+////            child: ClipRect(//裁切长方形
+////              child: BackdropFilter(//背景过滤器
+////                filter: ImageFilter.blur(sigmaX: 5.0,sigmaY: 5.0),//图片模糊过滤，横向竖向都设置5.0，直面意思就是横向竖向模糊拉伸度
+////                child: Opacity(//透明控件
+////                  opacity: 0.1,
+////                  child: Container(//容器组件
+////                    width: 1000.0,
+////                    height: 3000.0,
+////                    decoration: BoxDecoration(color: Colors.grey.shade100),//盒子装饰器，进行装饰，设置颜色为灰色
+////                    child: Center(
+////                      child: Text(
+////                        '约我请打电话110',
+////                        style: Theme.of(context).textTheme.display2,//设置字体theme
+////                      ),
+////                    ),
+////                  ),
+////                ),
+////              ),
+////            ),
+////          )
+////        ],
+////      ),
+//
+//    //todo 还可以这么写   直接在Stack的 childern里写一个底部图片，上面盖一层自适应的背景过滤器，在其内部在放一个文字或者不放或者放其他组件都可以，即可
+//      body: Stack(
 //        children: <Widget>[
-//          ConstrainedBox(//约束盒子组件，可以添加额外的限制条件到child上
-//            constraints: const BoxConstraints.expand(),//限制条件，可限制child的宽和高等
-//            //child: Image.network('https://img.lovebuy99.com/uploads/allimg/190807/15-1ZPGF354-50.jpg'),
-//            child: Image.network('http://qiniu.nightfarmer.top/恶龙咆哮.gif'),
+//          Image.network(
+//            'https://img.lovebuy99.com/uploads/allimg/190807/15-1ZPGF354-50.jpg',
+////            width: 1000.0,
+////            height: 1000.0,
 //          ),
-//          Center(
-//            child: ClipRect(//裁切长方形
-//              child: BackdropFilter(//背景过滤器
-//                filter: ImageFilter.blur(sigmaX: 5.0,sigmaY: 5.0),//图片模糊过滤，横向竖向都设置5.0，直面意思就是横向竖向模糊拉伸度
-//                child: Opacity(//透明控件
-//                  opacity: 0.1,
-//                  child: Container(//容器组件
-//                    width: 1000.0,
-//                    height: 3000.0,
-//                    decoration: BoxDecoration(color: Colors.grey.shade100),//盒子装饰器，进行装饰，设置颜色为灰色
-//                    child: Center(
-//                      child: Text(
-//                        '约我请打电话110',
-//                        style: Theme.of(context).textTheme.display2,//设置字体theme
-//                      ),
-//                    ),
-//                  ),
-//                ),
+//          BackdropFilter(
+//            filter: ImageFilter.blur(sigmaX: 5.0,sigmaY: 5.0),
+//            child: Container(
+//              color: Colors.blueAccent.withOpacity(0.1),
+////              width: 1000.0,
+////              height: 1000.0,
+//              child: Center(
+//                child: Text('要约吗？？？请拨 110',style: Theme.of(context).textTheme.display1,),
 //              ),
 //            ),
 //          )
 //        ],
 //      ),
+//
+//
+//    );
+//  }
+//
+//}
 
-    //todo 还可以这么写   直接在Stack的 childern里写一个底部图片，上面盖一层自适应的背景过滤器，在其内部在放一个文字或者不放或者放其他组件都可以，即可
+//TODO 再次练习高斯模糊效果
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class MaoBoLi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('高斯模糊效果',style: TextStyle(fontSize: 22.0,color: Colors.deepPurpleAccent),),backgroundColor: Colors.deepOrange,),
       body: Stack(
         children: <Widget>[
-          Image.network(
-            'https://img.lovebuy99.com/uploads/allimg/190807/15-1ZPGF354-50.jpg',
-//            width: 1000.0,
-//            height: 1000.0,
-          ),
+          Image.network('http://qiniu.nightfarmer.top/恶龙咆哮.gif',width: 1000.0,height: 1000.0,),
           BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 5.0,sigmaY: 5.0),
             child: Container(
-              color: Colors.blueAccent.withOpacity(0.1),
-//              width: 1000.0,
-//              height: 1000.0,
+              color: Colors.green.withOpacity(0.5),
+              width: 1000.0,
+              height: 1000.0,
               child: Center(
-                child: Text('要约吗？？？请拨 110',style: Theme.of(context).textTheme.display1,),
+                child: Text('要约吗？？？请拨 110',style: Theme.of(context).textTheme.display2,),
               ),
             ),
-          )
+          ),
+
         ],
       ),
-
-
     );
   }
-  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
+//import 'package:flutter/material.dart';
+//import 'MaoBoLi.dart';
+//
+//void main()=>runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'flutter_毛玻璃效果',
+//      theme: ThemeData(
+//        primarySwatch: Colors.deepOrange
+//      ),
+//      home: MaoBoLi(),
+//    );
+//  }
+//}
+
+//TODO 二次练习高斯模糊效果
 import 'package:flutter/material.dart';
 import 'MaoBoLi.dart';
 
@@ -7,9 +26,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter_毛玻璃效果',
+      title: '高斯模糊效果练习',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange
+        primarySwatch: Colors.blue,
       ),
       home: MaoBoLi(),
     );
